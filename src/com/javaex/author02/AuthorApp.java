7894d258ffbc8233954cf7ed75219c1f77388382
kimsy <duddl2102@naver.com>
2020-12-30 11:34:18 +0900

    2020/12/30 JDBC-DAO 수업내용, 실습 commit

---
+++ src/com/javaex/author02/AuthorApp.java
@@ -0,0 +1,73 @@
+package com.javaex.author02;
+
+import java.util.List;
+
+public class AuthorApp {
+
+	public static void main(String[] args) {
+		
+		AuthorDao authorDao = new AuthorDao();
+		List<AuthorVO> authorList = authorDao.getAuthorList();
+		
+		//int count = authorDao.AuthorInsert("테스트", "경북 영양"); //작가테이블에 추가
+		//System.out.println(count + "건 등록되었습니다.");
+		
+		
+		//등록(insert)
+		AuthorVO authorVo1 = new AuthorVO("이문열", "경북 영양");
+		authorDao.AuthorInsert(authorVo1);
+		
+		AuthorVO authorVo2 = new AuthorVO("박경리", "경상남도 통영");
+		authorDao.AuthorInsert(authorVo2);
+		
+		AuthorVO authorVo3 = new AuthorVO("유시민", "17대 국회의원");
+		authorDao.AuthorInsert(authorVo3);
+		/*
+		authorDao.AuthorInsert("이문열", "경북 영양"); //작가테이블에 추가
+		authorDao.AuthorInsert("박경리", "경상남도 통영"); //작가테이블에 추가
+		authorDao.AuthorInsert("유시민", "17대 국회의원"); //작가테이블에 추가
+		*/
+		
+		//리스트(select)
+		authorList = authorDao.getAuthorList();
+		
+		//for() 이용해서 리스트 전체 출력
+		System.out.println("=============작가리스트=============");
+		for(int i = 0; i < authorList.size(); i++) {
+			AuthorVO vo = authorList.get(i);
+			System.out.println(vo.getAuthorId() + ", " + vo.getAuthorName()
+								+ ", " + vo.getAuthorDesc());
+		}
+		
+		//삭제(delete)
+		authorDao.authorDelete(3);
+		
+		//리스트 출력
+		authorList = authorDao.getAuthorList();
+		//for() 이용해서 리스트 출력
+		
+		System.out.println("=============작가리스트=============");
+		for(int i = 0; i < authorList.size(); i++) {
+			AuthorVO vo = authorList.get(i);
+			System.out.println(vo.getAuthorId() + ", " + vo.getAuthorName()
+								+ ", " + vo.getAuthorDesc());
+		}
+	
+		//수정(update)
+		AuthorVO authorVo4 = new AuthorVO(2, "김경리", "제주도");
+		authorDao.authorUpdate(authorVo4);
+
+		//리스트(select)
+		authorList = authorDao.getAuthorList();
+				
+		//for() 이용해서 리스트 전체 출력
+		System.out.println("=============작가리스트=============");
+		for(int i = 0; i < authorList.size(); i++) {
+			AuthorVO vo = authorList.get(i);
+			System.out.println(vo.getAuthorId() + ", " + vo.getAuthorName()
+								+ ", " + vo.getAuthorDesc());
+		}
+		
+	}
+
+}
---
+++ src/com/javaex/book02/AuthorDao.java
@@ -0,0 +1,232 @@
+package com.javaex.book02;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthorDao {
+	//필드
+	// 0. import java.sql.*;
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+
+			
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String id = "webdb";
+	private String pw = "webdb";
+	
+	//생성자
+	
+	
+	//메소드-g/s
+	//디폴트 생성자 생략(다른 생성자 없음)
+	
+	
+	//메소드 - 일반
+	private void getConnection() {
+		try {
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName(driver);
+
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection(url, id, pw);
+			
+			System.out.println("[접속성공]");
+			
+		} catch (ClassNotFoundException e) {
+		    System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		}
+
+	}
+	
+	private void close() {
+		// 5. 자원정리
+	    try {
+	    	if (rs != null) {
+	            rs.close();
+	        }  
+	        if (pstmt != null) {
+	            pstmt.close();
+	        }
+	        if (conn != null) {
+	            conn.close();
+	        }
+	    } catch (SQLException e) {
+	        System.out.println("error:" + e);
+	    }
+	}
+	
+	
+	//작가 수정하기
+	public int authorUpdate(AuthorVO authorVo) {
+
+		int count = 0;
+		//DB접속
+		getConnection();
+		
+		try {
+		    // 3. SQL문 준비 / 바인딩 / 실행
+		    /*
+		    update author
+			set author_name = '김서영',
+			    author_desc = '경기도 수원시'
+			where author_id = 1;
+		    */
+			String query = "";
+			query += " update author         ";
+			query += " set author_name = ? , ";
+			query += "     author_desc = ?   ";
+			query += " where author_id = ?   ";
+			
+			System.out.println(query);
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, authorVo.getAuthorName());
+			pstmt.setString(2, authorVo.getAuthorDesc());
+			pstmt.setInt(3, authorVo.getAuthorId());
+			
+			count = pstmt.executeUpdate();
+			
+		    // 4.결과처리
+			System.out.println(count + " 건 수정");
+	
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		}
+		
+		//자원관리
+		close();
+		
+		return count;
+	}
+	//작가 삭제하기
+	public int authorDelete(int authorId) {
+	
+		int count = 0;
+
+		//DB접속
+		getConnection();
+		
+		try {
+		    // 3. SQL문 준비 / 바인딩 / 실행
+		    /*
+		    delete from author
+			where author_id = 1;
+		    */
+			String query = "";
+			query += "delete from author ";
+			query += "where author_id = ?";
+			
+			System.out.println(query);
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, authorId);
+			
+			count = pstmt.executeUpdate();
+			
+		    // 4.결과처리
+			System.out.println(count + " 건 삭제");
+
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		}
+		
+		//자원정리
+		close();
+		
+		return count;
+	}
+	
+	//작가테이블 리스트 가져오기
+	public List<AuthorVO> getAuthorList() {
+		
+		List<AuthorVO> authorList = new ArrayList<AuthorVO>();
+		
+		//DB접속
+		getConnection();
+
+		try {
+		    // 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " select  author_id,   ";
+			query += " 		   author_name, ";
+			query += "         author_desc  ";
+			query += " from author          ";
+			
+			System.out.println(query);
+
+			pstmt = conn.prepareStatement(query);
+			
+			rs = pstmt.executeQuery();
+		    
+		    // 4.결과처리
+			//rs에 있는 데이터를 List에 담아줘야함.(List<AuthorVO>로 구성)
+			while(rs.next()) {
+				int authorId = rs.getInt("author_id");
+				String authorName = rs.getString("author_name");
+				String authorDesc = rs.getString("author_desc");
+				
+				AuthorVO vo = new AuthorVO(authorId, authorName, authorDesc);
+				authorList.add(vo);
+			}
+
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		}
+		
+		//자원정리
+		close();
+		
+		return authorList;
+	}
+	
+	//작가 저장 기능
+	public int AuthorInsert(AuthorVO aurhorVo) {
+		
+		int count = 0;
+		
+		//DB접속
+		getConnection();
+		
+		try {
+		    // 3. SQL문 준비 / 바인딩 / 실행
+			/*
+			insert into author 
+			values (SEQ_AUTHOR_ID.nextval, '김서영', '경기도 수원시'); 
+			*/
+			
+			String query = "";
+			query += " insert into author ";
+			query += " values (SEQ_AUTHOR_ID.nextval, ?, ?) ";
+			
+			System.out.println(query);
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, aurhorVo.getAuthorName());
+			pstmt.setString(2, aurhorVo.getAuthorDesc());
+			
+			count = pstmt.executeUpdate();
+		    
+		    // 4.결과처리
+			System.out.println("[DAO] : " + count + " 건이 저장");
+
+		} catch (SQLException e) {
+		    System.out.println("error:" + e);
+		} 
+		
+		//자원정리
+		close();
+		
+		return count;
+
+	}
+	
+}
