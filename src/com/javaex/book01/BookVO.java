6b25e2d263b7008eab7c0ce40913c02ebd0bfb05
kimsy <duddl2102@naver.com>
2020-12-30 14:49:56 +0900

    상속부분 수정해서 commit

--- src/com/javaex/book01/BookVO.java
+++ src/com/javaex/book01/BookVO.java
@@ -1,6 +1,6 @@
 package com.javaex.book01;
 
-public class BookVO extends AuthorVO{
+public class BookVO  /*extends AuthorVO 잘못된 예 문법적으로는 가능*/{
 	
 	//필드
 	public int book_id;
@@ -8,14 +8,16 @@ public class BookVO extends AuthorVO{
 	public String pubs;
 	public String pub_date;
 	public int authorId;
+	public String authorName;
+	public String authorDesc;
 	
 	
 	//생성자
 	public BookVO() {}
 
 	public BookVO(int book_id, String title, String pubs, String pub_date, int authorId, String author_name, String author_desc) {
-		super.authorName = author_name;
-		super.authorDesc = author_desc;
+		this.authorName = author_name;
+		this.authorDesc = author_desc;
 		this.book_id = book_id;
 		this.title = title;
 		this.pubs = pubs;
@@ -79,6 +81,22 @@ public class BookVO extends AuthorVO{
 
 	public void setAuthorId(int authorId) {
 		this.authorId = authorId;
+	}
+	
+	public String getAuthorName() {
+		return authorName;
+	}
+
+	public void setAuthorName(String authorName) {
+		this.authorName = authorName;
+	}
+
+	public String getAuthorDesc() {
+		return authorDesc;
+	}
+
+	public void setAuthorDesc(String authorDesc) {
+		this.authorDesc = authorDesc;
 	}
 
 	//메소드-일반
